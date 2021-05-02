f36974bd43811fd186d2da1d4ca43498616a714f
kimhuy011199 <kimhuy011199@gmail.com>
2021-05-02 20:24:03 +0700

    create sign in screen

--- App.js
+++ App.js
@@ -1,12 +1,184 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import * as React from 'react';
+import { Image, Text, TextInput, View, TouchableOpacity, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/home'
+import TimekeeperScreen from './screens/timekeeper'
+import SalaryScreen from './screens/salary'
+import ComplaintScreen from './screens/complaint'
 import { styles } from './styles/styles'
 
-export default function App() {
+const AuthContext = React.createContext();
+
+function SplashScreen() {
   return (
-    <View style={styles.container}>
-      <Text>TIMEKEEPING SYSTEM</Text>
+    <View>
+      <Text>Loading...</Text>
     </View>
   );
 }
 
+function SignInScreen() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const { signIn } = React.useContext(AuthContext);
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.HomeContainer}>
+        <View style={styles.logoContainer}>
+          <Image 
+            source={require('./assets/logo.png')}
+            style={styles.logo}>
+          </Image>
+        </View>
+        <Text style={styles.welcome}>Welcome,</Text>
+        <Text style={styles.subWelcome}>Sign in to continue!</Text>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+          <TouchableOpacity 
+            style={styles.button}
+            onPress={() => signIn({ username, password })}>
+            <Text style={styles.buttonText}>SIGN IN</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App({ navigation }) {
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        // Restore token stored in `SecureStore` or any other encrypted storage
+        // userToken = await SecureStore.getItemAsync('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (data) => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async (data) => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
+  );
+
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        {
+          state.isLoading ? (
+            // We haven't finished checking for the token yet
+              <Stack.Screen name="Splash" component={SplashScreen} />
+          ) : state.userToken == null ? (
+            // No token found, user isn't signed in
+            <Stack.Navigator>
+            <Stack.Screen
+              name="SignIn"
+              component={SignInScreen}
+              options={{
+                title: 'Timekeeper Application',
+                // When logging out, a pop animation feels intuitive
+                animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+                headerShown: false
+              }}
+            />
+          </Stack.Navigator>
+          ) : (          
+            <Stack.Navigator
+              screenOptions={{
+              headerStyle: {
+                backgroundColor: '#24c48a',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              }, 
+              }}
+            >
+              <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+              <Stack.Screen name="ComplaintScreen" component={ComplaintScreen} options={{ title: 'New Complaint' }} />
+              <Stack.Screen name="SalaryScreen" component={SalaryScreen} options={{ title: 'Your Salary' }} />
+              <Stack.Screen name="TimekeeperScreen" component={TimekeeperScreen} options={{ title: 'Your Workdays' }} />
+            </Stack.Navigator> 
+          )
+        }
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+}
--- styles/styles.js
+++ styles/styles.js
@@ -5,7 +5,57 @@ export const styles = StyleSheet.create({
         padding: 30,
         backgroundColor: '#fff',
         flex: 1,
+    },
+    HomeContainer: {
+        padding: 30,
+        backgroundColor: '#fff',
+        flex: 1,
         justifyContent: 'center',
+    },
+    logoContainer: {
         alignItems: 'center',
+        marginBottom: '7%',
+        marginTop: '-25%'
+    },
+    logo: {
+        height: 72,
+        width: 58,
+    },
+    welcome: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#333',
+        textAlign: 'center',
+    },
+    subWelcome: {
+        color: '#aaa',
+        textAlign: 'center',
+        fontSize: 22,
+        paddingTop: '2%',
+    },
+    form: {
+        paddingVertical: '10%',
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#eee',
+        marginBottom: 12,
+        borderRadius: 8,
+        padding: 10,
+        fontSize: 14,
+    },
+    button: {
+        backgroundColor: '#24c48a',
+        borderRadius: 8,
+        width: '50%',
+        marginLeft: '25%',
+        marginTop: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        padding: 10,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 14,
     },
 })
