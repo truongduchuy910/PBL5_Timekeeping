28deb98ac57b064a5317767eeb3a6f6ab0d88509
kimhuy011199 <kimhuy011199@gmail.com>
2021-05-04 15:48:22 +0700

    create modal

--- screens/complaintModal.js
+++ screens/complaintModal.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { styles } from '../styles/styles'
+import { View } from 'react-native';
+import { styles } from '../styles/styles';
+import { Calendar } from 'react-native-calendars';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>COMPLAINT MODAL SCREEN</Text>
-    </View>
-  );
+export default function ComplaintModal({pickDate}) {
+    return (
+        <View style={styles.modalContainer}>
+            <Calendar
+                theme={{
+                    arrowColor: '#24c48a',
+                    todayTextColor: '#24c48a',
+                    textMonthFontWeight: '500',
+                    monthTextColor: '#24c48a',
+                    textDayHeaderFontSize: 14,
+                    textDayFontSize: 14,
+                    textDayHeaderFontWeight: '500',
+                    selectedDayBackgroundColor: '#24c48a',
+                    selectedDayTextColor: '#ffffff',
+                    textSectionTitleColor: '#b6c1cd',
+                }}
+                onDayPress={(date) => pickDate(date)}
+                firstDay={1}
+                style={styles.modalCalendar}
+            />
+        </View>
+    );
 }
-
